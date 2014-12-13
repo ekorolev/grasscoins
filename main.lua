2fbd4021c3447a2e81ee627306d773ed02bcb9c8
Евгений <ea.korolev@mail.ru>
2014-12-13 19:15:07 +0400

    leave animation

--- main.lua
+++ main.lua
@@ -3,11 +3,12 @@ function love.load()
 	IMAGE_coin = love.graphics.newImage("coins.png")
 	FONT_score = love.graphics.newFont("font.ttf", 20)
 
-	coins = {}
+	coins = {} -- Массив активных монет
+	leave_coins = {} -- Массив улетающих монет
 
 	score = 0
 	CoinsLive = 3
-	SpawnInterval = 1.5
+	SpawnInterval = 0.4
 end
 
 function love.draw()
@@ -16,6 +17,10 @@ function love.draw()
 
 	for i = 1, #coins do
 		love.graphics.draw(IMAGE_coin, coins[i].x, coins[i].y)
+	end
+
+	for i = 1, #leave_coins do
+		love.graphics.draw(IMAGE_coin, leave_coins[i].x, leave_coins[i].y)
 	end
 
 	love.graphics.setColor(255, 255, 255)
@@ -48,6 +53,40 @@ function love.update(dt)
 	for i = 1, #removable do
 		table.remove(coins, removable[i])
 	end
+
+	for i = 1, #leave_coins do
+		local coin = leave_coins[i]
+		if coin.x==coin.toX and coin.y==coin.toY then
+			table.remove(leave_coins, i)
+			break
+		end
+
+		local speed = 20
+
+		if  not(coin.x==coin.toX) then
+			if coin.x<coin.toX then
+				coin.x = coin.x + speed
+			else
+				coin.x = coin.x - speed
+			end
+		end
+
+		if not(coin.y==coin.toY) then
+			if coin.y<coin.toY then
+				coin.y = coin.y + speed
+			else
+				coin.y = coin.y - speed
+			end
+		end
+
+		if math.abs( coin.x - coin.toX ) < speed then
+			coin.x = coin.toX
+		end
+		if math.abs( coin.y - coin.toY ) < speed then
+			coin.y = coin.toY
+		end
+
+	end
 end
 
 function love.keypressed(key)
@@ -66,10 +105,11 @@ function love.mousepressed(x, y, button)
 			local cy = coins[i].y
 			if distance(x, y, cx+10, cy+10) < 20 then
 				score = score + 1
-				table.remove(coins, i)
+				coin_leaver(i)
 
 				if (score % 10==0) then
 					CoinsLive = CoinsLive - 0.1
+					SpawnInterval = SpawnInterval - 0.3
 				end
 				break
 			end
@@ -81,4 +121,11 @@ end
 
 function distance(x1, y1, x2, y2)
 	return math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) )
+end
+
+function coin_leaver(i)
+	coins[i].toX = 50
+	coins[i].toY = 30
+	table.insert(leave_coins, coins[i])
+	table.remove(coins, i)
 end
